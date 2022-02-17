1e5da7dc626e2a0b9b1290a70d497ba5942a3255
wwbarton <wbartonw5@gmail.com>
2022-02-17 11:51:41 -0700

    icon href

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -6,19 +6,17 @@ function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a href="#">
-              <i className="fa fa-facebook-square"></i>
+            <a href="https://github.com/wwbarton" target="_blank">
+              <i class="fa fa-github-square"></i>
             </a>
-            <a href="#">
+
+            <a href="mailto:wwalt.barton@gmail.com" target="_blank">
               <i className="fa fa-google-plus-square"></i>
             </a>
-            <a href="#">
-              <i className="fa fa-instagram"></i>
+            <a href="https://www.linkedin.com/in/wwbarton/" target="_blank">
+              <i class="fa fa-linkedin"></i>
             </a>
-            <a href="#">
-              <i className="fa fa-youtube-square"></i>
-            </a>
-            <a href="#">
+            <a href="https://twitter.com/wbartonw5" target="_blank">
               <i className="fa fa-twitter"></i>
             </a>
           </div>
