5f19c2c788928b4eaf9bf7952bdc1c1862f6d7ac
wwbarton <wbartonw5@gmail.com>
2022-02-17 17:40:10 -0700

    profile styling

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -6,35 +6,36 @@ function Profile() {
       <div className="profile-parent">
         <div className="profile-details">
           <div className="colz">
-            <a
-              href="https://github.com/wwbarton"
-              target="_blank"
-              rel="noopener"
-            >
-              <i class="fa fa-github-square"></i>
-            </a>
-
-            <a
-              href="mailto:wwalt.barton@gmail.com"
-              target="_blank"
-              rel="noopener"
-            >
-              <i className="fa fa-google-plus-square"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/wwbarton/"
-              target="_blank"
-              rel="noopener"
-            >
-              <i class="fa fa-linkedin"></i>
-            </a>
-            <a
-              href="https://twitter.com/wbartonw5"
-              target="_blank"
-              rel="noopener"
-            >
-              <i className="fa fa-twitter"></i>
-            </a>
+            <div className="colz-icon">
+              <a
+                href="https://github.com/wwbarton"
+                target="_blank"
+                rel="noopener"
+              >
+                <i class="fa fa-github fa-3x"></i>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/wwbarton/"
+                target="_blank"
+                rel="noopener"
+              >
+                <i class="fa fa-linkedin fa-3x"></i>
+              </a>
+              <a
+                href="mailto:wwalt.barton@gmail.com"
+                target="_blank"
+                rel="noopener"
+              >
+                <i className="fa fa-google-plus-square fa-3x"></i>
+              </a>
+              <a
+                href="https://twitter.com/wbartonw5"
+                target="_blank"
+                rel="noopener"
+              >
+                <i className="fa fa-twitter fa-3x"></i>
+              </a>
+            </div>
           </div>
 
           <div className="profile-details-name">
@@ -52,6 +53,18 @@ function Profile() {
             </span>
             <span className="profile-role-tagline">MERN Stack Dev</span>
           </div>
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire Me
+              {""}
+            </button>
+            <a href="#"></a> {/* resume */}
+            <button className="btn highlighted-btn">Get Resume</button>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
         </div>
       </div>
     </div>
