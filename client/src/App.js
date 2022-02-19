5a13ff6fd43a414f4f8d8500a67a3787417e4ea7
wwbarton <wbartonw5@gmail.com>
2022-02-18 21:37:32 -0700

    work section added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Home from "./PortfolioContainer/Home/Home";
+import Work from "./PortfolioContainer/Home/Work/Work";
 function App() {
   return (
     <div className="App">
       <Home />
+      <Work />
     </div>
   );
 }
